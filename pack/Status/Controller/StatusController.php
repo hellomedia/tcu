fb9a14ef1ed7406246b5d239983f6fb7e6f70745
Nicolas Sauveur <nicolas.sauveur@gmail.com>
2025-09-23 21:22:04 +0200

    Fix status controllers

--- pack/Status/Controller/StatusController.php
+++ pack/Status/Controller/StatusController.php
@@ -2,17 +2,19 @@
 
 namespace Pack\Status\Controller;
 
+use Admin\Controller\DashboardController;
 use App\Controller\BaseController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Option\EA;
 use Pack\Status\Chart\OpCacheChartProvider;
 use Pack\Status\Chart\RealpathCacheChartProvider;
 use Pack\Status\Model\OpCacheStatus;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route(path: '/status', name: 'controlroom_status_')]
+#[Route(path: '/status')]
 class StatusController extends BaseController
 {
-    #[Route(path: '/opcache', name: 'opcache')]
+    #[Route(path: '/opcache', name: 'admin_status_opcache', defaults: [EA::DASHBOARD_CONTROLLER_FQCN => DashboardController::class])]
     public function opcache(OpCacheChartProvider $charts): Response
     {
         $opCache = new OpCacheStatus();
@@ -31,7 +33,7 @@ class StatusController extends BaseController
         ]);
     }
 
-    #[Route(path: '/realpath_cache', name: 'realpath_cache')]
+    #[Route(path: '/realpath_cache', name: 'admin_status_realpath_cache', defaults: [EA::DASHBOARD_CONTROLLER_FQCN => DashboardController::class])]
     public function realpathCache(RealpathCacheChartProvider $charts): Response
     {
         $realpathChart = $charts->getRealpathCacheChart();
@@ -41,7 +43,7 @@ class StatusController extends BaseController
         ]);
     }
 
-    #[Route(path: '/phpinfo', name: 'phpinfo')]
+    #[Route(path: '/phpinfo', name: 'admin_status_phpinfo', defaults: [EA::DASHBOARD_CONTROLLER_FQCN => DashboardController::class])]
     public function phpinfo(): Response
     {
         ob_start();
